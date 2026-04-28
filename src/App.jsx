2cc35d6d5217329d1df7fe8af21a5815a19a70e7
jd24-sahur <elizaldejohndavid94@gmail.com>
2026-04-28 22:39:16 +0800

    basic landing screen

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+function App() {
+  return (
+    <div style={{
+      height: "100vh",
+      backgroundColor: "#f5f5f5",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "column",
+      fontFamily: "Arial"
+    }}>
+      
+      <h1 style={{
+        fontSize: "50px",
+        fontWeight: "bold",
+        background: "linear-gradient(to right, red, orange, yellow, green, blue, violet)",
+        WebkitBackgroundClip: "text",
+        WebkitTextFillColor: "transparent"
+      }}>
+        Grammatag
+      </h1>
+
+      <p>Coming Soon...</p>
+
+    </div>
+  );
+}
+
+export default App;
